2e6ef2793c3e2029fc2536f15e12d62e98d354a1
antu <antusaha970@gmail.com>
2023-03-26 19:24:20 +0600

    added customer review api

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = 5000;
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { MongoClient } = require("mongodb");
+const fileUpload = require("express-fileupload");
+const fs = require('fs-extra');
 require('dotenv').config();
 const uri = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.cfh8khq.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
@@ -27,21 +29,30 @@ async function run() {
   try {
     const database = client.db(`${process.env.DATABASE}`);
     const customerOrderCollection = database.collection('customerOrder');
+    const customerReviewCollection = database.collection('customerReview');
     console.log('Connected to Database');
 
 
     // Add order data
-    app.post('/addOrder',(req,res)=>{
-        const data = req.body;
-        customerOrderCollection.insertOne(data).then(result =>{
-            res.send(result.acknowledged);
-        })
+    app.post('/addOrder', (req, res) => {
+      const data = req.body;
+      customerOrderCollection.insertOne(data).then(result => {
+        res.send(result.acknowledged);
+      })
     })
 
     // API For getting customer order data
-    app.get('/orderList',(req,res)=>{
-      customerOrderCollection.find({email:req.query.email}).toArray().then(result=>{
+    app.get('/orderList', (req, res) => {
+      customerOrderCollection.find({ email: req.query.email }).toArray().then(result => {
         res.send(result);
+      })
+    })
+
+    // Customer Review 
+    app.post('/postReview', (req, res) => {
+      const review = req.body;
+      customerReviewCollection.insertOne(review).then(result => {
+        res.send(result.acknowledged);
       })
     })
 
